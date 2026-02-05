5224ac2201abdd29df84c00aefccd9be776dbff0
Felipe Batista <felipe09_2007@hotmail.com>
2026-02-05 19:03:33 +0000

    test: add `/fixtures/index.ts` and change fixtures filenames

---
+++ tests/fixtures/index.ts
@@ -0,0 +1,6 @@
+import { resolve } from "path";
+
+export const PDF_OK = resolve(__dirname, "ok.pdf");
+export const PDF_EMPTY = resolve(__dirname, "empty.pdf");
+export const PDF_LARGE_SIZE = resolve(__dirname, "large_size.pdf");
+export const NOT_PDF = resolve(__dirname, "not_pdf.txt");
